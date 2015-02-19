94703230d75fe8c04739d3665910f4c3552290fc
web user <web@savecode.ru>
2015-02-19 05:16:57 -0500

    features

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Codes::Application.configure do
+Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -12,14 +12,14 @@ Codes::Application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations
+  # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
   # Debug mode disables concatenation and preprocessing of assets.
@@ -27,17 +27,15 @@ Codes::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false #true
+  # Asset digests allow you to set far-future HTTP expiration dates on all assets,
+  # yet still be able to expire them through the digest params.
+  config.assets.digest = true
 
-  # SMTP server configuration
-  config.action_mailer.smtp_settings = {
-      :address => 'smtp.yandex.ru',
-      :port => 465,
-      :authentication => :login, # :plain, :login or :cram_md5
-      :user_name => 'admin',
-      :password => 'za9bali_so_vzlomom'
-  }
+  # Adds additional error checking when serving assets at runtime.
+  # Checks for improperly declared sprockets dependencies.
+  # Raises helpful error messages.
+  config.assets.raise_runtime_errors = true
 
+  # Raises error for missing translations
+  # config.action_view.raise_on_missing_translations = true
 end
