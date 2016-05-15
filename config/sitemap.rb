f496a329477d23fae420ce2ec65405b8958a01ed
KOPOJI <kopojiq@mail.ru>
2016-05-15 20:05:10 +0300

    sitemap alternate

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -22,24 +22,24 @@ SitemapGenerator::Sitemap.create do
   # Add all articles:
   #
 
-  add '/ru/codes/new.html'
+  add '/ru/codes/new.html', alternate: {href: '/en/codes/new.html', lang: :en}
   Code.find_each do |code|
-    add "/ru/codes/#{code.id}.html", :lastmod => code.updated_at
+    add "/ru/codes/#{code.id}.html", lastmod: code.updated_at, alternate: {href: "/en/codes/#{code.id}.html", lang: :en}
   end
 
-  add '/ru/attachments.html'
-  add '/ru/attachments/new.html'
+  add '/ru/attachments.html', alternate: {href: '/en/attachments.html', lang: :en}
+  add '/ru/attachments/new.html', alternate: {href: '/en/attachments/new.html', lang: :en}
   Attachment.find_each do |attach|
-    add "/ru/attachments/#{attach.id}.html", :lastmod => attach.updated_at
+    add "/ru/attachments/#{attach.id}.html", lastmod: attach.updated_at, alternate: {href: "/en/attachments/#{attach.id}.html", lang: :en}
   end
 
-  add '/ru/users.html'
-  add '/ru/user/login.html'
-  add '/ru/user/register.html'
-  add '/ru/user/secret/new.html'
+  add '/ru/users.html', alternate: {href: '/en/users.html', lang: :en}
+  add '/ru/user/login.html', alternate: {href: '/en/users/login.html', lang: :en}
+  add '/ru/user/register.html', alternate: {href: '/en/user/register.html', lang: :en}
+  add '/ru/user/secret/new.html', alternate: {href: '/en/user/secret/new.html', lang: :en}
   User.find_each do |u|
-    add "/ru/user/#{u.id}.html", :lastmod => u.updated_at
-    add "/ru/codes/find_by_user/#{u.id}.html" , :lastmod => u.updated_at
+    add "/ru/user/#{u.id}.html", lastmod: u.updated_at, alternate: {href: "/en/user/#{u.id}.html", lang: :en}
+    add "/ru/codes/find_by_user/#{u.id}.html" , lastmod: u.updated_at, alternate: {href: "/en/codes/find_by_user/#{u.id}.html", lang: :en}
   end
 
 end
