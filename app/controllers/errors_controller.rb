3df5e9ecf573f327518d8204bbe10809969ac4f2
KOPOJI <kopojiq@mail.ru>
2016-05-26 23:08:43 +0300

    fix locales

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,7 +2,7 @@ class ErrorsController < ApplicationController
 
   def show
     status_code = params[:code] || 500
-    flash.alert = I18n.t(status_code.to_s)
+    flash.alert = I18n.t("error_#{status_code}")
     render status_code.to_s, status: status_code
   end
 
