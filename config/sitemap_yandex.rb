c1f4efcc9f5b66e9f4aae95bb48b5f7010f6c29c
KOPOJI <kopojiq@mail.ru>
2017-04-15 17:24:42 +0300

    http

--- config/sitemap_yandex.rb
+++ config/sitemap_yandex.rb
@@ -1,5 +1,5 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = 'https://savecode.ru'
+SitemapGenerator::Sitemap.default_host = 'http://savecode.ru'
 
 SitemapGenerator::Sitemap.sitemaps_path = 'yandex'
 
